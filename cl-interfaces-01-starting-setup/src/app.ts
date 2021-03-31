c6dce7c031c0357386225f0f8034259bcd8c421d
razvan.rang <razvan.rang@safetech.ro>
2021-04-01 00:05:39 +0300

    added department class

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+class Department {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  describe(){
+    console.log('Department: ' + this.name);
+  }
+}
+
+const hr = new Department('HR');
+hr.describe();
+
+const hrCopy = {describe: hr.describe};
+
+hrCopy.describe();
+
+//describe
